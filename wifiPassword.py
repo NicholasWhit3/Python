4c6782200bfdcf5462718e02efa3f8b293f2e60c
Mykola Timchenko <64132625+NicholasWhit3@users.noreply.github.com>
2022-02-02 16:08:09 +0200

    Update wifiPassword.py

--- wifiPassword.py
+++ wifiPassword.py
@@ -4,20 +4,19 @@ import subprocess
 def extract_wifi_passwords():
     profiles_data = subprocess.check_output('netsh wlan show profiles').decode('utf-8').split('\n')
 
-    profiles = [i.split(':')[1].strip() for i in profiles_data if 'All user Profile' in i]
+    profiles = [i.split(':')[1].strip() for i in profiles_data if 'All User Profile' in i]
 
     for profile in profilies:
         profile_info = subprocess.check_output(f'netsh wlan show profile {profile} key=clear').decode('utf-8').split('\n')
 
         try:
-            password = [i.split(':')[1].strip() for i in profile_info if 'Key Content' in i]
+            password = [i.split(':')[1].strip() for i in profile_info if 'Key Content' in i][0]
         except IndexError:
             password = None
 
-        print(f'Profile: {profile}\nPassword: {password}\n{"#" * 20}')
-
+        
         with open(file='wifi_password.txt', mode='a', encoding='utf-8') as file:
-            file.write(f'Profile: {profile}\nPassword: {password}\n{"#" * 20}')
+            file.write(f'Profile: {profile}\nPassword: {password}\n{"#" * 20}\n')
 
 
 def main():
